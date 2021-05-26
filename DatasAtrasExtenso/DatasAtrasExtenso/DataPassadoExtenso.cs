3e34fd5cdfacb70b8f8622dae5cc0af7784727d5
Bleyk <50923316+pedro-ca@users.noreply.github.com>
2021-05-26 17:37:21 -0300

    Commit inicial e testes unitarios basicos

---
+++ DatasAtrasExtenso/DatasAtrasExtenso/DataPassadoExtenso.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DatasAtrasExtenso
+{
+    public class DataPassadoExtenso
+    {
+        private DateTime dataInicial;
+        private DateTime dataFinal;
+
+        public DataPassadoExtenso(DateTime dataInicial, DateTime dataFinal)
+        {
+            this.dataInicial = dataInicial;
+            this.dataFinal = dataFinal;
+        }
+
+        public string ConverterParaExtenso()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
