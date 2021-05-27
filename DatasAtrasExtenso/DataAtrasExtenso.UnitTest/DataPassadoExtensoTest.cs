6026056fa37906ee6eac3e90aa224db3c5423f68
Bleyk <50923316+pedro-ca@users.noreply.github.com>
2021-05-27 14:56:11 -0300

    Adicionado mais UnitTests e metodos de Receber Numeros

---
+++ DatasAtrasExtenso/DataAtrasExtenso.UnitTest/DataPassadoExtensoTest.cs
@@ -0,0 +1,171 @@
+using NUnit.Framework;
+using DatasAtrasExtenso;
+using System;
+
+namespace DataAtrasExtenso.UnitTest
+{
+    public class Tests
+    {
+        [TestCase]
+        public void Deve_RetornarExtenso_UmDiaAtras()
+        {
+            DateTime dataInicial = new DateTime(2021,05,25);    // 25/05/2021
+            DateTime dataFinal = new DateTime(2021,05,26);
+
+            DataPassadoExtenso dataExtenso = new DataPassadoExtenso(dataInicial, dataFinal);
+
+            string respostaAtual = dataExtenso.ConverterParaExtenso();
+            string respostaEsperada = "um dia atrás";
+            Assert.AreEqual(respostaAtual, respostaEsperada);
+        }
+
+        [TestCase]
+        public void Deve_RetornarExtenso_DoisDiasAtras()
+        {
+            DateTime dataInicial = new DateTime(2021, 05, 24);    // 24/05/2021
+            DateTime dataFinal = new DateTime(2021, 05, 26);
+
+            DataPassadoExtenso dataExtenso = new DataPassadoExtenso(dataInicial, dataFinal);
+
+            string respostaAtual = dataExtenso.ConverterParaExtenso();
+            string respostaEsperada = "dois dias atrás";
+            Assert.AreEqual(respostaAtual, respostaEsperada);
+        }
+
+        [TestCase]
+        public void Deve_RetornarExtenso_UmaSemanaAtras()
+        {
+            DateTime dataInicial = new DateTime(2021, 05, 19);    // 19/05/2021
+            DateTime dataFinal = new DateTime(2021, 05, 26);
+
+            DataPassadoExtenso dataExtenso = new DataPassadoExtenso(dataInicial, dataFinal);
+
+            string respostaAtual = dataExtenso.ConverterParaExtenso();
+            string respostaEsperada = "uma semana atrás";
+            Assert.AreEqual(respostaAtual, respostaEsperada);
+        }
+
+        [TestCase]
+        public void Deve_RetornarExtenso_DuasSemanasAtras()
+        {
+            DateTime dataInicial = new DateTime(2021, 05, 12);    // 12/05/2021
+            DateTime dataFinal = new DateTime(2021, 05, 26);
+
+            DataPassadoExtenso dataExtenso = new DataPassadoExtenso(dataInicial, dataFinal);
+
+            string respostaAtual = dataExtenso.ConverterParaExtenso();
+            string respostaEsperada = "duas semanas atrás";
+            Assert.AreEqual(respostaAtual, respostaEsperada);
+        }
+
+        [TestCase]
+        public void Deve_RetornarExtenso_DuasSemanasUmDiaAtras()
+        {
+            DateTime dataInicial = new DateTime(2021, 05, 11);    // 19/05/2021
+            DateTime dataFinal = new DateTime(2021, 05, 26);
+
+            DataPassadoExtenso dataExtenso = new DataPassadoExtenso(dataInicial, dataFinal);
+
+            string respostaAtual = dataExtenso.ConverterParaExtenso();
+            string respostaEsperada = "duas semanas e um dia atrás";
+            Assert.AreEqual(respostaAtual, respostaEsperada);
+        }
+
+        [TestCase]
+        public void Deve_RetornarExtenso_DoisMesesDoisDiasAtras()
+        {
+            DateTime dataInicial = new DateTime(2021, 05, 25);    //24/04/2021
+            DateTime dataFinal = new DateTime(2021, 05, 26);
+
+            DataPassadoExtenso dataExtenso = new DataPassadoExtenso(dataInicial, dataFinal);
+
+            string respostaAtual = dataExtenso.ConverterParaExtenso();
+            string respostaEsperada = "dois meses e dois dias atrás";
+
+            Assert.AreEqual(respostaAtual, respostaEsperada);
+        }
+
+        [TestCase]
+        public void Deve_RetornarExtenso_DoisMesesUmaSemanaAtras()
+        {
+            DateTime dataInicial = new DateTime(2021, 04, 19);    // 19/04/2021
+            DateTime dataFinal = new DateTime(2021, 05, 26);
+
+            DataPassadoExtenso dataExtenso = new DataPassadoExtenso(dataInicial, dataFinal);
+
+            string respostaAtual = dataExtenso.ConverterParaExtenso();
+            string respostaEsperada = "dois meses e uma semana atrás";
+            Assert.AreEqual(respostaAtual, respostaEsperada);
+        }
+
+        [TestCase]
+        public void Deve_RetornarExtenso_UmAnoAtras()
+        {
+            DateTime dataInicial = new DateTime(2020, 05, 25);    //26/05/2011
+            DateTime dataFinal = new DateTime(2021, 05, 26);
+
+            DataPassadoExtenso dataExtenso = new DataPassadoExtenso(dataInicial, dataFinal);
+
+            string respostaAtual = dataExtenso.ConverterParaExtenso();
+            string respostaEsperada = "um ano atrás";
+
+            Assert.AreEqual(respostaAtual, respostaEsperada);
+        }
+
+        [TestCase]
+        public void Deve_RetornarExtenso_UmAnoUmDiaAtras()
+        {
+            DateTime dataInicial = new DateTime(2020, 05, 25);    //26/05/2011
+            DateTime dataFinal = new DateTime(2021, 05, 26);
+
+            DataPassadoExtenso dataExtenso = new DataPassadoExtenso(dataInicial, dataFinal);
+
+            string respostaAtual = dataExtenso.ConverterParaExtenso();
+            string respostaEsperada = "um ano e um dia atrás";
+
+            Assert.AreEqual(respostaAtual, respostaEsperada);
+        }
+
+        [TestCase]
+        public void Deve_RetornarExtenso_UmAnoUmaSemanaAtras()
+        {
+            DateTime dataInicial = new DateTime(2020, 05, 19);    //19/05/2011
+            DateTime dataFinal = new DateTime(2021, 05, 26);
+
+            DataPassadoExtenso dataExtenso = new DataPassadoExtenso(dataInicial, dataFinal);
+
+            string respostaAtual = dataExtenso.ConverterParaExtenso();
+            string respostaEsperada = "um ano e uma semana atrás";
+
+            Assert.AreEqual(respostaAtual, respostaEsperada);
+        }
+
+        [TestCase]
+        public void Deve_RetornarExtenso_UmAnoDoisMesesDoisDiasAtras()
+        {
+            DateTime dataInicial = new DateTime(2020, 05, 25);    //24/04/2020
+            DateTime dataFinal = new DateTime(2021, 05, 26);
+
+            DataPassadoExtenso dataExtenso = new DataPassadoExtenso(dataInicial, dataFinal);
+
+            string respostaAtual = dataExtenso.ConverterParaExtenso();
+            string respostaEsperada = "um ano e dois meses e dois dias atrás";
+
+            Assert.AreEqual(respostaAtual, respostaEsperada);
+        }
+
+        [TestCase]
+        public void Deve_RetornarExtenso_DezAnosAtras()
+        {
+            DateTime dataInicial = new DateTime(2011, 05, 26);    //26/05/2011
+            DateTime dataFinal = new DateTime(2021, 05, 26);
+
+            DataPassadoExtenso dataExtenso = new DataPassadoExtenso(dataInicial, dataFinal);
+
+            string respostaAtual = dataExtenso.ConverterParaExtenso();
+            string respostaEsperada = "dez anos atrás";
+
+            Assert.AreEqual(respostaAtual, respostaEsperada);
+        }
+    }
+}
--- DatasAtrasExtenso/DatasAtrasExtenso/DataPassadoExtenso.cs
+++ DatasAtrasExtenso/DatasAtrasExtenso/DataPassadoExtenso.cs
@@ -19,5 +19,82 @@ namespace DatasAtrasExtenso
         {
             throw new NotImplementedException();
         }
+
+        private string ReceberUnidades(string numero)
+        {
+            switch (numero)
+            {
+                case "1":
+                    return "um ";
+                case "2":
+                    return "dois ";
+                case "3":
+                    return "três ";
+                case "4":
+                    return "quatro ";
+                case "5":
+                    return "cinco ";
+                case "6":
+                    return "seis ";
+                case "7":
+                    return "sete ";
+                case "8":
+                    return "oito ";
+                case "9":
+                    return "nove ";
+                default:
+                    return "";
+            }
+        }
+
+        private string ReceberDezenas(string numero)
+        {
+            switch (numero)
+            {
+                case "10":
+                    return "dez ";
+                case "11":
+                    return "onze ";
+                case "12":
+                    return "doze ";
+                case "13":
+                    return "treze ";
+                case "14":
+                    return "catorze ";
+                case "15":
+                    return "quinze ";
+                case "16":
+                    return "dezesseis ";
+                case "17":
+                    return "dezessete ";
+                case "18":
+                    return "dezoito ";
+                case "19":
+                    return "dezenove ";
+                case "20":
+                    return "vinte ";
+                case "30":
+                    return "trinta ";
+                case "40":
+                    return "quarenta ";
+                case "50":
+                    return "cinquenta ";
+                case "60":
+                    return "sessenta ";
+                case "70":
+                    return "setenta ";
+                case "80":
+                    return "oitenta ";
+                case "90":
+                    return "noventa ";
+
+                default:
+                    if (double.Parse(numero) > 0)
+                    {
+                        return ReceberDezenas(numero.Substring(0, 1) + "0") + "e " + ReceberUnidades(numero.Substring(1));
+                    }
+                    return "";
+            }
+        }
     }
 }
